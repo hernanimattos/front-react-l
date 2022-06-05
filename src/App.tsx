68d1a5fe6f60fee11ca7c2c37c8801cfe5b042c0
Hernani Luiz de mattos <hernani.luiz@gmail.com>
2022-06-04 22:31:47 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputForm, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const changeHandler = (e: any) => {
+    setForm({ ...inputForm, [e.target.name]: e.target.value });
+  };
+
+  const handleAuth = (event: any) => {
+    event.preventDefault();
+
+    console.log(event.target.elements.email.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='App'>
+        <div className='image-login'>image</div>
+        <form className='form-login' onSubmit={(event) => handleAuth(event)}>
+          <h2>Login</h2>
+          <input
+            id='email'
+            name='email'
+            type='text'
+            placeholder='e-mail'
+            autoComplete='on'
+            className='input'
+            value={inputForm.email}
+            onChange={(e) => changeHandler(e)}
+            required
+          />
+          <input
+            id='password'
+            name='password'
+            type='password'
+            placeholder='password'
+            autoComplete='off'
+            value={inputForm.password}
+            onChange={(e) => changeHandler(e)}
+          />
+          <button type='submit' value='enter'>
+            Vai
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
